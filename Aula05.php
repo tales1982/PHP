b040d2a865bcaded9cae2e584054ec96fd5ba5ea
Tales Lima <tales_lima_1982@hotmail.com>
2022-06-23 11:05:16 +0200

    update

---
+++ Aula05.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exemplo Php</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+
+</head>
+<body>
+    
+</body>
+</html>
+<div>
+    <h2>Concatenação</h2>
+    <?php
+  $idade = 40;
+  $nome = "Tales";
+  echo $nome." Tem ".$idade." anos de idade.";
+?>
+
+</div>
