9c8c33e949d4e6672669a0861d0621c8b93c658a
Tales Lima <tales_lima_1982@hotmail.com>
2022-07-01 21:35:11 +0200

    Unario

--- 06_Operadores_Unarios.php
+++ 06_Operadores_Unarios.php
@@ -18,7 +18,7 @@
         $tipo = $_GET["op"];
         echo "Os valores passados foram $n1 e $n2<br>";
         $r = ($tipo == "s") ? $n1 + $n2 : $n1 * $n2;
-        echo "O resultado sera $r"
+        echo "O resultado sera $r";
         ?>
     </div>
 
