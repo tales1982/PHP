3289e8c70cedb4f1f7a35ef7e6f4cdd3ec414ede
Tales Lima <tales_lima_1982@hotmail.com>
2022-07-03 19:46:15 +0200

    Unario

--- 06_Operadores_Unarios.php
+++ 06_Operadores_Unarios.php
@@ -13,11 +13,18 @@
     <div>
         <h2>Unario</h2>
         <?php
+        /*
+        $nota1 = $_GET["n1"];
+        $nota2 = $_GET["n2"];
+        $media = ($nota1 + $nota2)/2;
+        echo "A media entre $nota1 e $nota2 e $media <br/>";
+        echo "A situação do aluno é ".(($media < 6)?"<strong>Reprovado</strong>":"<strong>Aprovado</strong>");
+        */
         $n1 = $_GET["a"];
         $n2 = $_GET["b"];
         $tipo = $_GET["op"];
-        echo "Os valores passados foram $n1 e $n2<br>";
-        $r = ($tipo == "s") ? $n1 + $n2 : $n1 * $n2;
+        echo "Os valores passados foram $n1 e $n2<br/>";
+        $r = ($tipo == "s") ? $n1 + $n2 : $n1 * $n2; 
         echo "O resultado sera $r";
         ?>
     </div>
